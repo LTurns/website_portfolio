47d487e89470605a64ff51fb064d79d1e60c8108
LTurns <lizzieturney@gmail.com>
2020-07-13 11:40:25 +0100

    Routes completed for the backend

---
+++ backend/database/db.js
@@ -0,0 +1,7 @@
+// This is all we need to do to declare a database:
+// projeggdb is the database name. We put it in an export method
+// so it becomes available elsewhere.
+
+module.exports = {
+  db: "mongodb://localhost:27017/projeggdb",
+};
---
+++ backend/models/task.js
@@ -0,0 +1,22 @@
+// Models is where we create the mongoDB schemas for interacting with our database.
+// More research into difference between schemas and tables.
+// In node, models refer to the database tables & schemas. 
+
+
+// Here we simply declared a name and email field along with their respective data types in task Schema. (Mirroring the form on the front-end to structure what will go into the database when input by user)
+
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema; 
+
+let taskSchema = new Schema)({
+    name: {
+        type: String
+    }, 
+    email: {
+        type: String
+    }
+}, {
+    collection: 'tasks'
+})
+
+module.exports = mongoose.model('Task', studentSchema)
