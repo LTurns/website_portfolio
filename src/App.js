20aed7c16d2ddf2cbb99f86a1be82e8a753f014e
LTurns <lizzieturney@gmail.com>
2020-07-17 10:49:01 +0100

    Altered file structure more

--- src/App.js
+++ src/App.js
@@ -3,11 +3,10 @@ import "./App.css";
 import "bootstrap/dist/css/bootstrap.css";
 import Home from "./components/home";
 import Portfolio from "./components/portfolio";
-import Contact from "./components/contact";
+// import Contact from "./components/contact";
 import About from "./components/about"
 import Navigation from './components/navigation';
 import "./home.css"
-
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
 function App() {
@@ -17,7 +16,7 @@ function App() {
 
       <Route path="/" exact component={Home}/>
       <Route path="/portfolio" exact component={Portfolio}/>
-      <Route path="/contact" exact component={Contact}/>
+      {/* <Route path="/contact" exact component={Contact}/> */}
       <Route path="/about" exact component={About}/>
     </Router>
   );
