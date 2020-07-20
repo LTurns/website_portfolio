cd86f7d4841c291edff42840cf7bd70cbc795dca
LTurns <lizzieturney@gmail.com>
2020-07-21 00:20:12 +0100

    Email now working independently on backend server

--- backend/index.js
+++ backend/index.js
@@ -1,21 +1,59 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express'); //needed to launch server
+const cors = require('cors'); //needed to disable sendgrid security
+const nodemailer = require('nodemailer');
+const bodyParser = require('body-parser');
 
-var app = express();
+const app = express(); //alias from the express function
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors()); 
+app.use(bodyParser.json());
+var jsonParser = bodyParser.json()
+// means the request body of html is being passed through json 
+// We know that in this instance it is being passed as json because the header (think about cors) 
+// has the content type of json file. The content-type on contact.js is what enabled the req.body to come back
+// with the correct content, instead of simply "undefined".
+app.use(bodyParser.urlencoded({extended:false}));
+// means the request body of the html (react) is being passed through url (API)
+app.listen(4000, () => console.log('Example app listening on port 4000!'))
 
-var mailRouter = require('./routes/mail');
-// route setup
-app.use('/api/mail', mailRouter);
+app.get('/', (req, res) => {
+    res.send("Welcome to my form"); 
+});
 
-app.use(function(req, res, next) {
-    next(createError(404));
-  });
+app.post('/api/form', jsonParser, (req, res)=> {
+    var data = req.body 
+    let smtpTransport = nodemailer.createTransport({
+        service:'Gmail', 
+        port: 4000, 
+        auth: {
+            user: 'lizziesoftware@gmail.com', 
+            pass: 'princeamos1'
+        }
+    })
+
+    let mailOptions={
+        from: data.email,
+        to: 'lizziesoftware@gmail.com', 
+        subject: 'Message from website',
+        html: `<h5> Message below: </h5>
+        
+        <ul> 
+        <li> First Name: ${data.fName} </li>
+        <li> Last Name: ${data.lName} </li>
+        <li> Email: ${data.email} </li>
+        <li> Message: ${data.message} </li>
+        </ul> `
+    }; 
+
+    smtpTransport.sendMail(mailOptions, (error, response) => {
+        if (error){
+            res.send(error)
+        } else (
+            res.send('success!')
+        )
+    })
+
+    smtpTransport.close();
+})
+
+
