1fc4ac4b9446b9c328bfa3c062238781a08df9f5
LTurns <lizzieturney@gmail.com>
2020-07-21 00:31:14 +0100

    Deleted unnecessary files

--- backend/routes/mail.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-var mailController = require('../controllers/mail');
-
-router.post('/submit', mailController.Create);
-
-module.exports = router;
